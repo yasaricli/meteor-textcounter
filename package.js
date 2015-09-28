c582c960d99c6a1936f2e0f617349519fcb391d3
Yasar icli <hello@yasaricli.com>
2015-09-28 12:10:51 +0300

    documentation describe removed

--- package.js
+++ package.js
@@ -2,8 +2,7 @@ Package.describe({
   name: 'yasaricli:textcounter',
   version: '0.0.1',
   summary: 'A jQuery plugin which provides a countdown of text in an input field.',
-  git: 'https://github.com/yasaricli/meteor-textcounter.git',
-  documentation: 'README.md'
+  git: 'https://github.com/yasaricli/meteor-textcounter.git'
 });
 
 Package.onUse(function(api) {
